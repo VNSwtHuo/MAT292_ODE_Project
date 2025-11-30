f8ca122345da0e8cff34f998fddcb8f1896df334
Vanessa Huo <vanessa.wt.huo@gmail.com>
2025-11-30 11:31:55 -0500

    Coded sensitivity analysis for neural ode

--- models/NeuralODE/generate_data.r
+++ models/NeuralODE/generate_data.r
@@ -10,9 +10,11 @@ see(mod)
 param(mod)
 init(mod)
 
+mod <- mod %>% param(CL = 50, VC = 3.5, Q = 8, VP = 10, Q2 = 2, VP2=20, KA1=0.195, KA2=6.02736, KM=5.57)
+
 
 sim_data <- mod %>%
-    ev(amt = 100, cmt = 2) %>% # dose into compartment 2
+    ev(amt = 400, cmt = 2) %>% # dose into compartment 2
     mrgsim(end = 48, delta = 0.5)
 plot()
 
@@ -20,3 +22,4 @@ head(sim_data)
 
 # Save to CSV
 write.csv(sim_data, "mrgsolve_simulation.csv", row.names = FALSE)
+
